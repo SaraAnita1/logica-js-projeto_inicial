b625d90013bb864c4c5ff3cd60aa5ec2fd9cb3c3
Sara Anita <sara.anita.desa@gmail.com>
2024-09-12 19:19:01 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+alert('Boas vindas ao nosso site!');
+let nome = "Lua";
+let idade = perguntarIdade;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let mensagemDeErro = alert('Erro! Preencha todos os campos')
+prompt('Qual o Seu nome?')
+let perguntarIdade = prompt('Qual é a sua idade?')
+
+
+if(idade >= 18) {
+    alert('pode tirar a habilitação')
+}
